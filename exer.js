f40e906e242bbe6ab5b5b53bed92064d777496c5
leandro de oliveira <leandro.pinheiro0717@gmail.com>
2026-01-30 17:50:00 -0300

    Comentando o Javascript

--- exer.js
+++ exer.js
@@ -1,12 +1,18 @@
+// seleciona o formulário e o h3
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
 
 
+// adiciona o ouvinte do evento submit
 frm.addEventListener("submit" , (e) => {
-    e.preventDefault()
+    e.preventDefault() // impede o recarregamento da página
 
+
+    // obtém o valor digitado e converte para número
     const numero = Number(frm.inNumero.value)
  
+   
+    // verifica se é par ou ímpar
     if ( numero % 2 === 0) {
          resp.innerText = `O número ${numero} é Par`
     } else {
